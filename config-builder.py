22784cefc68b2d8f6473b44be5e8e5b7021c6346
torrented <wallace.ryan10@gmail.com>
2013-08-02 00:17:36 -0700

    Updated config-builder.py

--- config-builder.py
+++ config-builder.py
@@ -9,9 +9,9 @@ import os
 # Getting Device Spec's
 
 def devicespec():
-  a = raw_input('\nCROSS_COMPILE=~')
-  b = raw_input('LOCAL_BUILD_DIR=~')
-  c = raw_input('TARGET_DIR=~')
+  a = raw_input('\nCROSS_COMPILE=~/')
+  b = raw_input('LOCAL_BUILD_DIR=~/')
+  c = raw_input('TARGET_DIR=~/')
   d = raw_input('SYSTEM_PARTITION=/dev/block/mmcblk0p')
   e = raw_input('DEFCONFIG=')
   f = raw_input('N_CORES=')
@@ -19,17 +19,24 @@ def devicespec():
 # Print Device Spec's
 
   print '\n=====================================\n'
-  print 'CROSS_COMPILE=~' + a
+  print 'CROSS_COMPILE=~/' + a
   print 'HOST_CC=gcc'
-  print 'LOCAL_BUILD_DIR=~' + b 
-  print 'TARGET_DIR=~' + c
+  print 'LOCAL_BUILD_DIR=~/' + b 
+  print 'TARGET_DIR=~/' + c
   print 'SYSTEM_PARTITION=\"/dev/block/mmcblk0p' + d + '\"'
   print 'DEFCONFIG=' + e
   print "\nFLASH_BOOT='write_raw_image(\"/tmp/boot.img\", \"boot\")'"
   print '\n# ----\n'
   print 'N_CORES=' + f
   print 'VERSION=' + g
-  print '\n# ----'
+  print '\n# ----\n\n'
+  print'BANNER=`cat <<EOF'
+  print'ui_print("**********************************************");'
+  print'ui_print("*                                            *");'
+  print'ui_print("  VERSION: ' + g + '                     ");'
+  print'ui_print("*                                            *");'
+  print'ui_print("**********************************************");'
+  print'EOF`'
   print '\n=====================================\n'
 
 # Generate kerneltools-build-config
@@ -51,17 +58,24 @@ def devicespec():
        nf = 'kerneltools-build-config'
   
     outfile = open(nf, 'w')
-    outfile.write('CROSS_COMPILE=~' + a)
+    outfile.write('CROSS_COMPILE=~/' + a)
     outfile.write('\nHOST_CC=gcc')
-    outfile.write('\nLOCAL_BUILD_DIR=~' + b)
-    outfile.write('\nTARGET_DIR=~' + c)
+    outfile.write('\nLOCAL_BUILD_DIR=~/' + b)
+    outfile.write('\nTARGET_DIR=~/' + c)
     outfile.write('\nSYSTEM_PARTITION=\"/dev/block/mmcblk0p' + d + '\"')
     outfile.write('\nDEFCONFIG=' + e)
     outfile.write("\n\nFLASH_BOOT='write_raw_image(\"/tmp/boot.img\", \"boot\")'")
     outfile.write('\n\n# ----')
     outfile.write('\n\nN_CORES=' + f)
     outfile.write('\nVERSION=' + g)
-    outfile.write('\n\n# ----')
+    outfile.write('\n\n# ----\n\n')
+    outfile.write('BANNER=`cat <<EOF\n')
+    outfile.write('ui_print("**********************************************");\n')
+    outfile.write('ui_print("*                                            *");\n')
+    outfile.write('ui_print("  VERSION: ' + g + '                     ");\n')
+    outfile.write('ui_print("*                                            *");\n')
+    outfile.write('ui_print("**********************************************");\n')
+    outfile.write('EOF`')
     outfile.close()
     sys.exit
 
