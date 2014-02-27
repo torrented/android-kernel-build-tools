2791568a7a6a8755d82703023f10c1fea348f315
torrented <wallace.ryan10@gmail.com>
2014-02-26 17:43:04 -0800

    Updated Script

--- config-builder.py
+++ config-builder.py
@@ -10,66 +10,81 @@ import commands
 # Getting the Device Spec's
 
 def devicespec():
-  a = raw_input('\nCROSS_COMPILE=~/')
-  b = raw_input('LOCAL_BUILD_DIR=~/')
-  c = raw_input('TARGET_DIR=~/')
-  d = raw_input('SYSTEM_PARTITION=/dev/block/mmcblk0p')
-  e = raw_input('DEFCONFIG=')
-  f = raw_input('N_CORES=')
-  g = raw_input('VERSION=')
+  cross_compile   = raw_input('\nCROSS_COMPILE=~/')
+  local_build_dir = raw_input('LOCAL_BUILD_DIR=~/')
+  target_dir      = raw_input('TARGET_DIR=~/')
+  partition       = raw_input('SYSTEM_PARTITION=/dev/block/mmcblk0p')
+  defconfig       = raw_input('DEFCONFIG=')
+  cores           = raw_input('N_CORES=')
+  version         = raw_input('VERSION=')
 
 # Print the Device Spec's
 
   print '\n=====================================\n'
-  print 'CROSS_COMPILE=~/' + a
+  print 'CROSS_COMPILE=~/' + cross_compile
   print 'HOST_CC=gcc'
-  print 'LOCAL_BUILD_DIR=~/' + b 
-  print 'TARGET_DIR=~/' + c
-  print 'SYSTEM_PARTITION=\"/dev/block/mmcblk0p' + d + '\"'
-  print 'DEFCONFIG=' + e
+  print 'LOCAL_BUILD_DIR=~/' + local_build_dir 
+  print 'TARGET_DIR=~/' + target_dir
+  print 'SYSTEM_PARTITION=\"/dev/block/mmcblk0p' + partition + '\"'
+  print 'DEFCONFIG=' + defconfig
   print "\nFLASH_BOOT='write_raw_image(\"/tmp/boot.img\", \"boot\")'"
   print '\n# ----\n'
-  print 'N_CORES=' + f
-  print 'VERSION=' + g
+  print 'N_CORES=' + cores
+  print 'VERSION=' + version
   print '\n# ----\n\n'
   print 'BANNER=`cat <<EOF'
   print 'ui_print("**********************************************");'
   print 'ui_print("*                                            *");'
-  print 'ui_print("  VERSION: ' + g + '                     ");'
+  print 'ui_print("  VERSION: ' + version + '                    ");'
   print 'ui_print("*                                            *");'
   print 'ui_print("**********************************************");'
   print 'EOF`'
   print '\n=====================================\n'
 
 # Generate kerneltools-build-config file
-  
-  answer1 = raw_input('\nDoes this Look Correct? Y or N? ')
-  if answer1 == 'n':
+
+  input = raw_input('\nDoes this Look Correct? y/n? ')
+  if input != 'y':
     main()
   else:
-    nf = 'kerneltools-build-config'
-    outfile = open(nf, 'w')
-    outfile.write('CROSS_COMPILE=~/' + a +
+    new_file = 'kerneltools-build-config'
+
+    outfile = open(new_file, 'w')
+    outfile.write('CROSS_COMPILE=~/' + cross_compile +
                   '\nHOST_CC=gcc' +
-                  '\nLOCAL_BUILD_DIR=~/' + b +
-                  '\nTARGET_DIR=~/' + c +
-                  '\nSYSTEM_PARTITION=\"/dev/block/mmcblk0p' + d + '\"' +
-                  '\nDEFCONFIG=' + e +
+                  '\nLOCAL_BUILD_DIR=~/' + local_build_dir +
+                  '\nTARGET_DIR=~/' + target_dir +
+                  '\nSYSTEM_PARTITION=\"/dev/block/mmcblk0p' + partition + '\"' +
+                  '\nDEFCONFIG=' + defconfig +
                   "\n\nFLASH_BOOT='write_raw_image(\"/tmp/boot.img\", \"boot\")'" +
                   '\n\n# ----' +
-                  '\n\nN_CORES=' + f +
-                  '\nVERSION=' + g +
+                  '\n\nN_CORES=' + cores +
+                  '\nVERSION=' + version +
                   '\n\n# ----\n\n' +
                   'BANNER=`cat <<EOF\n' +
                   'ui_print("**********************************************");\n' +
                   'ui_print("*                                            *");\n' +
-                  'ui_print("  VERSION: ' + g + '                     ");\n' +
+                  'ui_print("  VERSION: ' + version + '                    ");\n' +
                   'ui_print("*                                            *");\n' +
                   'ui_print("**********************************************");\n' +
                   'EOF`')
     outfile.close()
-    commands.getstatusoutput('mv ' + nf + ' ~/' + b)
-    print '\nSaved config file to: ' + '~/' + b + '/' + nf + '\n'
+
+    if os.path.isdir(local_build_dir):
+        commands.getstatusoutput('mv ' + new_file + ' ~/' + local_build_dir)
+    else:
+        commands.getstatusoutput('mkdir ' + '~/' + local_build_dir)
+        commands.getstatusoutput('mv ' + new_file + ' ~/' + local_build_dir)
+
+    print '\nSaved config file to: ' + '~/' + local_build_dir + '/' + new_file
+
+# Open directory containing source
+
+    input1 = raw_input('\nOpen top level directory of kernel source? y/n? ')
+    if input1 != 'y':
+	exit()
+    else:
+	commands.getstatusoutput('xdg-open ' + '~/' + local_build_dir)
 
 def main():
   devicespec()
